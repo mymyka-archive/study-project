4e39069e85c72d9cc1a0673211871262c0f8720c
Mykyta Kasianenko <kasianenkomykyta@gmail.com>
2023-10-26 18:43:44 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <v-bottom-navigation :elevation="24" grow>
+      <v-btn value="recent">
+        <v-icon>mdi-history</v-icon>
+
+        <span>Recent</span>
+      </v-btn>
+
+      <v-btn value="favorites">
+        <v-icon>mdi-heart</v-icon>
+
+        <span>Favorites</span>
+      </v-btn>
+
+      <v-btn value="nearby">
+        <v-icon>mdi-map-marker</v-icon>
+
+        <span>Nearby</span>
+      </v-btn>
+    </v-bottom-navigation>
+    <div class="gap-5">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/catalog">Home</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
